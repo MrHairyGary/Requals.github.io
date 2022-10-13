6e854dbf3221aac31da2f3998e2cf3bba4764fc0
Requals <86450119+Requals@users.noreply.github.com>
2022-10-13 09:39:32 -0400

    Add classes for different sections

--- docs/eteranical/main.js
+++ docs/eteranical/main.js
@@ -1,2 +1,28 @@
+class Game{
+  constructor(){}
+  
+  static element = document.querySelector(".game");
+}
 
-console.log("Loaded!")
+class Settings{
+  constructor(){}
+  
+  static element = document.querySelector(".settings");
+}
+
+class GameConsole{
+  constructor({history = []}){
+    this.history = [];
+  }
+  
+  static element = document.querySelector(".console");
+  
+  //IPad cannot look at console
+  log(message){
+    this.history.push(message)
+  }
+}
+
+const mainConsole = new GameConsole();
+const settings = new Settings();
+mainConsole.log("Loaded!")
