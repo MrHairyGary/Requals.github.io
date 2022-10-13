e9be8589ec99124945e5fc00df64acac7a600648
Requals <86450119+Requals@users.noreply.github.com>
2022-10-13 11:36:07 -0400

    Delete utilities.js

--- docs/eteranical/utilities.js
+++
@@ -1,6 +0,0 @@
-// Developer console ingame (Console is not available on IPad.)
-console.ipadhistory = []
-// logm means Log Menu
-console.logm = function(ttl){ // ttl means Text to Log
-  ipadhistory.push(ttl)
-}
