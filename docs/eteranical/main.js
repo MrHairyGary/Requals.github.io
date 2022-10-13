62aeccc4224de1ccebedf283c07b15af8b861afd
Requals <86450119+Requals@users.noreply.github.com>
2022-10-13 13:22:30 -0400

    Update main.js

--- docs/eteranical/main.js
+++ docs/eteranical/main.js
@@ -2,7 +2,7 @@
 class GameInstance{
   constructor(){
   }
-  static element = document.querySelector(".game");
+  element = document.querySelector(".game");
   hide(){element.style.display="none"}
   show(){element.style.display="block"}
   
@@ -10,7 +10,7 @@ class GameInstance{
 class SettingsInstance{
   constructor(){
   }
-  static element = document.querySelector(".settings");
+  element = document.querySelector(".settings");
   hide(){element.style.display="none"}
   show(){element.style.display="block"}
   
@@ -20,7 +20,7 @@ class GameConsole{
   constructor({history = []}){
     this.history = history;
   }
-  static element = document.querySelector(".console");
+  element = document.querySelector(".console");
   hide(){element.style.display="none"}
   show(){element.style.display="block"}
   //IPad cannot look at console
