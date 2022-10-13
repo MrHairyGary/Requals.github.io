734c18cca246df05cc9299af80b695c9e4524c08
Requals <86450119+Requals@users.noreply.github.com>
2022-10-13 06:42:35 -0400

    Create utilities.js

---
+++ docs/eteranical/utilities.js
@@ -0,0 +1,6 @@
+// Developer console ingame (Console is not available on IPad.)
+console.ipadhistory = []
+// logm means Log Menu
+console.logm = function(ttl){ // ttl means Text to Log
+  ipadhistory.push(ttl)
+}
